1bbe02d8607bb6f09cb5151c79148fe4387c8351
Lucc <Lucc@b210-madsen-nuc2.otatest01.cu-lte-pg0.madsen.powderwireless.net>
2020-07-17 13:32:43 -0600

    add test rx with Qt GUI

--- pbr_prog/rx_test.py
+++ pbr_prog/rx_test.py
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Custom Rx
+# Title: Not titled yet
+# Author: Lucc
 # GNU Radio version: 3.8.0.0-rc2
 
 from distutils.version import StrictVersion
@@ -24,7 +25,6 @@ from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
-from gnuradio import blocks
 from gnuradio import gr
 import sys
 import signal
@@ -35,12 +35,12 @@ from gnuradio import uhd
 import time
 from gnuradio import qtgui
 
-class custom_rx(gr.top_block, Qt.QWidget):
+class rx_test(gr.top_block, Qt.QWidget):
 
-    def __init__(self, bandwidth=0, devtype=MyB210, freq=2400000000.0, freq_offset=0, gain=20):
-        gr.top_block.__init__(self, "Custom Rx")
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet")
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Custom Rx")
+        self.setWindowTitle("Not titled yet")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -58,7 +58,7 @@ class custom_rx(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "custom_rx")
+        self.settings = Qt.QSettings("GNU Radio", "rx_test")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -69,34 +69,25 @@ class custom_rx(gr.top_block, Qt.QWidget):
             pass
 
         ##################################################
-        # Parameters
-        ##################################################
-        self.bandwidth = bandwidth
-        self.devtype = devtype
-        self.freq = freq
-        self.freq_offset = freq_offset
-        self.gain = gain
-
-        ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 1000000
+        self.samp_rate = samp_rate = 32000
 
         ##################################################
         # Blocks
         ##################################################
         self.uhd_usrp_source_0 = uhd.usrp_source(
-            ",".join(("", 'devtype')),
+            ",".join(("", "")),
             uhd.stream_args(
                 cpu_format="fc32",
                 args='',
                 channels=[],
             ),
         )
-        self.uhd_usrp_source_0.set_center_freq(freq, 0)
-        self.uhd_usrp_source_0.set_gain(gain, 0)
-        self.uhd_usrp_source_0.set_antenna('TX/RX', 0)
-        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(2.675e9, 0)
+        self.uhd_usrp_source_0.set_gain(20, 0)
+        self.uhd_usrp_source_0.set_antenna('RX2', 0)
+        self.uhd_usrp_source_0.set_samp_rate(1e6)
         self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec())
         self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
             1024, #size
@@ -148,53 +139,18 @@ class custom_rx(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '~/OTA/qpsk_4000_05.txt', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_time_sink_x_0, 0))
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "custom_rx")
+        self.settings = Qt.QSettings("GNU Radio", "rx_test")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
-
-    def get_bandwidth(self):
-        return self.bandwidth
-
-    def set_bandwidth(self, bandwidth):
-        self.bandwidth = bandwidth
-        self.uhd_usrp_source_0.set_bandwidth(self.bandwidth, 0)
-
-    def get_devtype(self):
-        return self.devtype
-
-    def set_devtype(self, devtype):
-        self.devtype = devtype
-
-    def get_freq(self):
-        return self.freq
-
-    def set_freq(self, freq):
-        self.freq = freq
-        self.uhd_usrp_source_0.set_center_freq(self.freq, 0)
-
-    def get_freq_offset(self):
-        return self.freq_offset
-
-    def set_freq_offset(self, freq_offset):
-        self.freq_offset = freq_offset
-
-    def get_gain(self):
-        return self.gain
-
-    def set_gain(self, gain):
-        self.gain = gain
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -202,36 +158,17 @@ class custom_rx(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
-        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
 
 
-def argument_parser():
-    parser = ArgumentParser()
-    parser.add_argument(
-        "-b", "--bandwidth", dest="bandwidth", type=eng_float, default="0.0",
-        help="Set RX Bandwidth [default=%(default)r]")
-    parser.add_argument(
-        "-f", "--freq", dest="freq", type=eng_float, default="2.4G",
-        help="Set Default Frequency [default=%(default)r]")
-    parser.add_argument(
-        "-o", "--freq-offset", dest="freq_offset", type=eng_float, default="0.0",
-        help="Set RX Frequency Offset  [default=%(default)r]")
-    parser.add_argument(
-        "-g", "--gain", dest="gain", type=eng_float, default="20.0",
-        help="Set Default Gain [default=%(default)r]")
-    return parser
 
-
-def main(top_block_cls=custom_rx, options=None):
-    if options is None:
-        options = argument_parser().parse_args()
+def main(top_block_cls=rx_test, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
         Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
 
-    tb = top_block_cls(bandwidth=options.bandwidth, freq=options.freq, freq_offset=options.freq_offset, gain=options.gain)
+    tb = top_block_cls()
     tb.start()
     tb.show()
 
