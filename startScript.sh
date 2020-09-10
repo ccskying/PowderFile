4ea01b08c4fe1b01887f3e4252d6f2db9fd36b47
ccskying <ccskying@126.com>
2020-09-09 23:32:51 -0400

    modifying start script

--- startScript.sh
+++ startScript.sh
@@ -2,12 +2,19 @@
 
 # Start script for Powder profile OAI_profile
 
+# Clone the python scripts written before
+
 git clone https://github.com/ccskying/PowderFile
 
-if 1 then
+# Only for the pc of X310, we need to run the following command
+# Remember to reboot the X310 after new image loaded into its FPGA
 
-sudo uhd_images_downloader
-sudo uhd_image_loader --args="type=x300,addr=192.168.40.2"
-wget https://codeload.github.com/EttusResearch/uhd/zip/release_003_010_003_000 -O uhd.zip
-unzip uhd.zip
-sudo cp -Rv uhd-release_003_010_003_000/host/include/uhd/rfnoc /usr/share/uhd/
+if [ rel=$(echo $HOSTNAME | grep "pc") ]; then
+    # Load image for X310
+    sudo uhd_images_downloader
+    sudo uhd_image_loader --args="type=x300,addr=192.168.40.2"
+    # Fix the missing of xml file
+    wget https://codeload.github.com/EttusResearch/uhd/zip/release_003_010_003_000 -O uhd.zip
+    unzip uhd.zip
+    sudo cp -Rv uhd-release_003_010_003_000/host/include/uhd/rfnoc /usr/share/uhd/
+fi
