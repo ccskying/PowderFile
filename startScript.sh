5cc3d81e5088b9388ee091048e43b56eee691a7b
ccskying <ccskying@126.com>
2020-09-09 23:47:50 -0400

    modifying start script

--- startScript.sh
+++ startScript.sh
@@ -4,12 +4,14 @@
 
 # Clone the python scripts written before
 
-git clone https://github.com/ccskying/PowderFile
+#git clone https://github.com/ccskying/PowderFile
 
 # Only for the pc of X310, we need to run the following command
 # Remember to reboot the X310 after new image loaded into its FPGA
 
-if [ rel=$(echo $HOSTNAME | grep "pc") ]; then
+if [ $(echo $HOSTNAME | grep "pc") ]; then
+echo "pc"
+: '
     # Load image for X310
     sudo uhd_images_downloader
     sudo uhd_image_loader --args="type=x300,addr=192.168.40.2"
@@ -17,4 +19,5 @@ if [ rel=$(echo $HOSTNAME | grep "pc") ]; then
     wget https://codeload.github.com/EttusResearch/uhd/zip/release_003_010_003_000 -O uhd.zip
     unzip uhd.zip
     sudo cp -Rv uhd-release_003_010_003_000/host/include/uhd/rfnoc /usr/share/uhd/
+'
 fi
